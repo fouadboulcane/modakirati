3c751df9ec6bd00477bf6b2f928f234c296b0d74
fouadlee <fouadlee9@gmail.com>
2023-11-12 23:53:48 +0100

    first commit

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -38,7 +38,7 @@ const DocumentIdPage = ({
     return (
       <div>
         {/* <Cover.Skeleton /> */}
-        <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
           <div className="space-y-4 pl-8 pt-4">
             <Skeleton className="h-14 w-[50%]" />
             <Skeleton className="h-4 w-[80%]" />
@@ -57,7 +57,7 @@ const DocumentIdPage = ({
   return ( 
     <div className="pb-40">
       <Cover url={document.coverImage} />
-      <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-20">
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar initialData={document} />
         <Editor
           onChange={onChange}
